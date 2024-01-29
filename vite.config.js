18b6aa741e524265eb5c5a1bffc5e8dd68692df1
Franck polla <fpolla640@gmail.com>
2024-01-29 09:44:38 +0200

    i modified the vite.config file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Todoapp/",
   build: {
     outDir: "dist", // Change this to match the desired directory name
   },
